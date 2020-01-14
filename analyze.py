80293110728dce3c58d5a98004833bec36515f96
Khalid Hamad <khalidazhmd@gmail.com>
2020-01-14 13:51:06 +0800

    handle csv and excel files - plot the histogram using barh

--- analyze.py
+++ analyze.py
@@ -1,20 +1,7 @@
-def get_grades(grades_file):
-    """(opened_file) -> list of floats
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
-    Return a list of the grades from the grades_file
 
-    Preconditions: grades_file is a csv file
-    """
-
-    grades_file.readline() # Skip the header
-    grades = []
-
-    for row in grades_file:
-        row = row.split(',')
-        grade = row[-1]
-        grades.append(float(grade.strip()))
-
-    return grades
 
 def get_grade_ranges():
     """
@@ -27,17 +14,52 @@ def get_grade_ranges():
     return ranges
 
 
-def get_grades_frequency(grades):
-    """
+def get_frequency(data_set):
+    """ (pandas dataframe) -> list of int
+
+    Returns a list of integers that represents the frequency of grades in
+    a certain interval
 
     """
 
-    freq = [0] * 11
+    grades = data_set["grade"] # Will not get modified so no need to copy
+    freq = []
 
-    for grade in grades:
-        grade = int(grade // 10)
-        freq[grade] += 1
+    for i in range(11):
+        grade_count = ((grades // 10) == i).sum()
+        freq.append(grade_count)
 
     return freq
 
 
+def get_grade_max(data_set):
+    """(pandas dataframe) -> tuple([list of matric numbers], grade)
+
+    """
+
+    max_grade = data_set["grade"].max(axis=0)
+    print(max_grade)
+    studnts_max_grade = data_set[data_set["grade"] == max_grade]
+    studnts_max_grade = studnts_max_grade["matric number"].to_list()
+
+    return studnts_max_grade, max_grade
+
+
+def write_report(avg_grade, median_grade, max_grade, students_max_grade):
+
+    print(
+f"""avg mark is {avg_grade},
+Grades has a median of {median_grade},
+heighest mark is {max_grade}, scored by:
+{', '.join(students_max_grade)}."""
+)
+
+
+def plot_hist(bins, grade_count, mean_grade):
+
+    plt.style.use("seaborn")
+    fig, ax = plt.subplots()
+    # plt.barh(bins, grade_count)
+    ax.barh(bins, grade_count)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.show()
