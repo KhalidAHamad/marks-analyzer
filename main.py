f0cf70bd11c8edbc38fcdc0dffc631a8d13cdd6d
Khalid Hamad <khalidazhmd@gmail.com>
2020-02-11 15:18:57 +0300

    merge pandas branch

--- main.py
+++ main.py
@@ -36,14 +36,6 @@ analyze.write_report(avg_grade, median_grade, max_grade, students_max_grade)
 
 grade_count = analyze.get_frequency(grades)
 grade_ranges = analyze.get_grade_ranges()
-# plt.barh(grade_ranges, grade_count)
-# plt.show()
-analyze.plot_hist(grade_ranges, grade_count, avg_grade)
-# read a file and turn the grades into a list
-# grades = analyze.get_grades(grades_file)
-# print(grades)
 
-# # create a grades range
-# grade_ranges = analyze.get_grade_ranges()
-# print(grade_ranges)
+analyze.plot_hist(grade_ranges, grade_count, avg_grade)
 
